f5b0e33c674d64dd06b6f4ec7189634e95d19eca
NeX0Zz <den.nickol1406@gmail.com>
2024-02-14 19:30:24 +0300

    Initial Commit

---
+++ GalleryApp/GalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryAppApp.swift
+//  GalleryApp
+//
+//  Created by Денис Николаев on 14.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct GalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
